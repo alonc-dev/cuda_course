965d4a30c3a820415255267cfa2a4293527dfbac
alonc-dev <alonc.dev@gmail.com>
2024-12-26 10:05:55 +0200

    update

--- 01-hello-gpu.cu
+++ 01-hello-gpu.cu
@@ -1,37 +1,27 @@
 #include <stdio.h>
 
-
-void helloCPU()
+__global__ void printSuccessForCorrectExecutionConfiguration()
 {
-  printf("Hello from the CPU.\n");
-}
 
-/*
- * Refactor the `helloGPU` definition to be a kernel
- * that can be launched on the GPU. Update its message
- * to read "Hello from the GPU!"
- */
-
-void helloGPU()
-{
-  printf("Hello also from the CPU.\n");
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  printf("%d) ThIdx.x=%d, BlkIdx.x=%d  - ",i, threadIdx.x, blockIdx.x);
+  
+  if(blockIdx.x == 5 && threadIdx.x == 3)
+  {
+    printf("Success!\n");
+  } else {
+    printf("Failure.\n");
+  }
 }
 
 int main()
 {
-
-  helloCPU();
-
   /*
-   * Refactor this call to `helloGPU` so that it launches
-   * as a kernel on the GPU.
+   * Update the execution configuration so that the kernel
+   * will print `"Success!"`.
    */
 
-  helloGPU();
+  printSuccessForCorrectExecutionConfiguration<<<6, 4>>>();
+  cudaDeviceSynchronize();
 
-  /*
-   * Add code below to synchronize on the completion of the
-   * `helloGPU` kernel completion before continuing the CPU
-   * thread.
-   */
 }
